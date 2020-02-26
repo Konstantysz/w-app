9977292130cf7004e85ed25cd2f55b639c951139
Konstantysz <kostekszumigaj@gmail.com>
2020-02-26 23:49:37 +0100

    Updates

---
+++ precache-manifest.3e3e59641663af0353b39a1cd378adc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b29307c30c7b0dde9c0a6508a179568",
+    "url": "/w-app/index.html"
+  },
+  {
+    "revision": "c48f834a6ffef2f6f945",
+    "url": "/w-app/static/css/main.6f58afb6.chunk.css"
+  },
+  {
+    "revision": "516285708c94535250ab",
+    "url": "/w-app/static/js/2.eee193d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/w-app/static/js/2.eee193d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48f834a6ffef2f6f945",
+    "url": "/w-app/static/js/main.77144276.chunk.js"
+  },
+  {
+    "revision": "ca8582c8fff6c005e9b2",
+    "url": "/w-app/static/js/runtime-main.39a4a916.js"
+  },
+  {
+    "revision": "592d66e04c1f8163a4dde2b2ae576dbc",
+    "url": "/w-app/static/media/weather.592d66e0.jpg"
+  }
+]);
